e950dd4cc83b7bcfd62cdd1fd42c88588c32fb21
somaraghava <raghavasoma22@gmail.com>
2017-02-14 14:39:23 +0530

    Add files via upload

---
+++ hashcode1.rb
@@ -0,0 +1,25 @@
+age_hash ={ }
+name=" "
+input=" "
+age=0
+
+puts "enter name,age (or) just ENTER to quit"
+input=gets.chomp
+
+while input!=" " do
+  (name,age)=input.split(",")
+  age=age.to_i
+
+  age_hash[name] = age
+
+puts "the hash content is:"
+
+age_hash.each do |key,value|
+  puts  "#{key} is #{age} years old"
+
+input =" "
+
+end
+end
+
+
